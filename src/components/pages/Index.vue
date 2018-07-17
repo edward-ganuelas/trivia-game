4a2f413855b28a04c350248901d992ccdbaf7772
Ganuelas, Edward (TOR-MCL) <Edward.Ganuelas@maclaren.com>
2018-07-17 11:00:11 -0400

    index page

---
+++ src/components/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="index">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
